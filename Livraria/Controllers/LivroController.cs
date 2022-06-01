513169de51baaeadbfb87511af1538ed6e0c4b1a
Gabriel <gabrielsousa5190@gmail.com>
2022-06-01 17:35:55 -0300

    criando models e controller do livro

---
+++ Livraria/Controllers/LivroController.cs
@@ -0,0 +1,39 @@
+﻿using Livraria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Livraria.Controllers
+{
+    public class LivroController : ControllerBase
+    {
+        public List<Livro> livros = new List<Livro>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaLivro(Livro livro)
+        {
+            livros.Add(livro);
+            livro.Id = id++;
+            return CreatedAtAction(nameof(PesquisaLivroPorId), new { id = livro.Id }, livro);
+        }
+
+        [HttpGet]
+        public IActionResult PesquisaLivro()
+        {
+            return Ok(livros);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult PesquisaLivroPorId(int id)
+        {
+            Livro livro = livros.FirstOrDefault(l => l.Id == id);
+
+            if (livro != null)
+            {
+                return Ok(livro);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Livraria/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Livraria.Models
+{
+    public class Livro 
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Required]
+        public string Genero { get; set; }
+    }
+}
